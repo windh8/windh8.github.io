f585fe2deb12c69398f92df53f6be964a1d2712e
Heaten Mistry <windH800000000@gmail.com>
2018-03-12 21:35:07 -0400

    Update
    New 'About Me' section (HTML) using new font (CSS), & tweaked jQuery function for header (jQuery)

--- script/jQueryFunctions.js
+++ script/jQueryFunctions.js
@@ -1,7 +1,7 @@
 $(function(){
         $(document).scroll(function(){
           $("#author-name").animate({top:"12px", left:"35px", fontSize:"25px" }, 200);
-          $("nav a").animate({top:"15px", left:"0px", fontSize:"20px" }, 175);
+          $("nav a").animate({top:"15px", left:"0px", fontSize:"20px" }, 200);
           $("header").animate({height:"50px"}, 200);
           $(".space").animate({height:"100px"}, 200);
         });
